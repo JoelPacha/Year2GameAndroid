626755dd5eeb2846224855530b0e44a285fefd89
PachaAfghan <joel.matin2@gmail.com>
2022-04-12 15:12:48 +0200

    update blocs

--- app/src/main/java/com/example/projet/Blocs.kt
+++ app/src/main/java/com/example/projet/Blocs.kt
@@ -1,19 +1,20 @@
 package com.example.projet
 
+import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.RectF
 import java.lang.Math.abs
 
-abstract class Blocs(x1: Float, y1: Float, x2: Float, y2: Float){
+abstract class Blocs(x1: Float,y1: Float,x2: Float, y2: Float){
     val largeur = abs(x2-x1)
     val hauteur = abs(y2-y1)
 
     val r = RectF(x1, y1, x2, y2)
     val paint = Paint()
     abstract fun reactionBalle(b: Balle)
-    abstract fun drawbloc()
-
-
-
+    fun drawbloc(canvas: Canvas, couleur:  ){
+        paint.color = Color.
+    }
 
 }
---
+++ app/src/main/java/com/example/projet/Parois.kt
@@ -0,0 +1,21 @@
+package com.example.projet
+
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.graphics.RectF
+
+class Parois(x1: Float, y1: Float, x2: Float, y2: Float): Blocs(x1, y1, x2, y2){
+    override fun reactionBalle(b: Balle) {
+        if (RectF.intersects(this.r,b.r )){
+            b.changeDirection()
+            }
+        }
+    }
+
+    override fun drawbloc(canvas: Canvas) {
+        val this.paint.color = Color.
+
+    }
+
+
+}
