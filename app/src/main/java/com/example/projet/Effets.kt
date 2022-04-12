10d4a55c24b3e08c5e4fa12e004d7ceb261e3fe7
Thomas Syti <thomas.syti@gmail.com>
2022-04-12 14:58:19 +0200

    Classe Effets.2

--- app/src/main/java/com/example/projet/Effets.kt
+++ app/src/main/java/com/example/projet/Effets.kt
@@ -1,4 +1,15 @@
 package com.example.projet
 
-class Effets {
+import android.graphics.*
+import java.util.*
+
+class Effets (x1 : Float, y1 : Float, val diametre: Float) {
+    val r = RectF(x1 ,y1, x1 + diametre, y1 + diametre)
+    val random = Random()
+    val paint = Paint()
+    var color = Color.argb(255, random.nextInt(256),
+        random.nextInt(256), random.nextInt(256))
+    abstract fun vitesseballe()
+    abstract fun tailleplateforme()
+
 }
