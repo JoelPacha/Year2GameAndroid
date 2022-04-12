6160e3b1f2e1b38210dc85483cc60afa18e27caf
PachaAfghan <joel.matin2@gmail.com>
2022-04-12 14:42:04 +0200

    abstractclass blocs

---
+++ app/src/main/java/com/example/projet/Blocs.kt
@@ -0,0 +1,18 @@
+package com.example.projet
+
+import android.graphics.Paint
+import android.graphics.RectF
+import java.lang.Math.abs
+
+abstract class Blocs(x1: Float, y1: Float, x2: Float, y2: Float){
+    val largeur = abs(x2-x1)
+    val hauteur = abs(y2-y1)
+
+    val r = RectF(x1, y1, x2, y2)
+    val paint = Paint()
+    abstract fun reactionBalle(b: Balle)
+    abstract fun drawbloc()
+
+
+
+}
