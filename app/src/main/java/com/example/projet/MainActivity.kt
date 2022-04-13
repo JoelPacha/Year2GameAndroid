8084e6d6225bfea1da0e592ee8d5c4b9077ecd92
Thomas Syti <thomas.syti@gmail.com>
2022-04-13 14:08:42 +0200

    update MainActivity.kt

--- app/src/main/java/com/example/projet/MainActivity.kt
+++ app/src/main/java/com/example/projet/MainActivity.kt
@@ -4,8 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var drawingView: DrawingView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawingView = findViewById<DrawingView>(R.id.vMain)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        drawingView.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        drawingView.resume()
     }
 }
