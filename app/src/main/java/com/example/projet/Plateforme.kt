63dede2064284509711938b9fdc819cee16e955b
fd06 <103423277+fd06@users.noreply.github.com>
2022-04-13 14:06:18 +0200

    Classe plateforme

---
+++ app/src/main/java/com/example/projet/Plateforme.kt
@@ -0,0 +1,35 @@
+package com.example.projet
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class Plateforme (x1:Float, y1: Float, x2: Float, y2: Float, ): Blocs(x1, y1, x2, y2), Bouge {
+    var v = 2.0F
+    var dx : Int
+
+    for (i in range 0..10)
+
+    init {
+
+    }
+
+    override fun reactionBalle(b: Balle) {
+        TODO("Not yet implemented")
+    }
+
+    override fun drawbloc() {
+        TODO("Not yet implemented")
+    }
+
+    override fun bouge(canvas: Canvas?) {
+        r.offset(v * dx)
+        draw(canvas)
+    }
+
+
+
+}
