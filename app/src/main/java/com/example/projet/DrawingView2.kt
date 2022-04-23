08a3131cb9cc5705494ef3b78dca45cbe2936a8e
Thomas Syti <thomas.syti@gmail.com>
2022-04-23 13:12:10 +0200

    grosse modif

---
+++ app/src/main/java/com/example/projet/DrawingView2.kt
@@ -0,0 +1,75 @@
+package com.example.projet
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+
+class DrawingView2 @JvmOverloads constructor (context: Context, attributes: AttributeSet? = null, defStyleAttr: Int = 0): SurfaceView(context, attributes,defStyleAttr), SurfaceHolder.Callback,Runnable {
+    lateinit var canvas: Canvas
+    val backgroundPaint = Paint()
+    lateinit var thread: Thread
+    var drawing: Boolean = true
+
+    init {
+        backgroundPaint.color = Color.WHITE
+    }
+
+    override fun onSizeChanged(w: Int,h: Int,oldw: Int,oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        val canvasH = (h - 500).toFloat()
+        val canvasW = (w - 25).toFloat()
+    }
+
+    fun pause() {
+        drawing = false
+        thread.join()
+    }
+
+    fun resume() {
+        drawing = true
+        thread = Thread(this)
+        thread.start()
+    }
+
+    fun draw() {
+        if (holder.surface.isValid) {
+            canvas = holder.lockCanvas()
+            canvas.drawRect(0F, 0F, canvas.getWidth()*1F,
+                canvas.getHeight()*1F, backgroundPaint)
+            holder.unlockCanvasAndPost(canvas)
+        }
+    }
+
+    override fun onTouchEvent(e: MotionEvent): Boolean {
+        when (e.action) {
+            MotionEvent.ACTION_DOWN -> {
+                val x = e.rawX.toInt() - 100
+                val y = e.rawY.toInt() - 500
+            }
+        }
+        return true
+    }
+
+    override fun run() {
+        while (drawing) {
+            draw()
+        }
+    }
+
+    override fun surfaceChanged(
+        holder: SurfaceHolder, format: Int,
+        width: Int, height: Int) {
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        thread = Thread(this)
+        thread.start()
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        thread.join()
+    }
+}
