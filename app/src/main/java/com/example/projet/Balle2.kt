53b21a42b03d1aa375a0d9bf624d7550c5b26a88
fd06 <103423277+fd06@users.noreply.github.com>
2022-05-07 17:14:36 +0200

    niveau 1 terminé

--- app/src/main/java/com/example/projet/Balle2.kt
+++ app/src/main/java/com/example/projet/Balle2.kt
@@ -9,7 +9,7 @@ class Balle2( x:Float, y: Float, diametre: Float,var vie:Int): Ovni2(x, y, diame
     override var dx = rand()
     override var dy = rand()
     override var VitesseOvni = 500f
-    override val color = Color.rgb(49,110,51)
+    override val color = Color.rgb(55,142,191)
 
     var vitesse_initiale = VitesseOvni
 
