ae941d4a4574d6ba8cf7c978b28776c8e65875f8
fd06 <103423277+fd06@users.noreply.github.com>
2022-05-07 14:18:08 +0200

    couleur up

--- app/src/main/java/com/example/projet/Malus.kt
+++ app/src/main/java/com/example/projet/Malus.kt
@@ -4,7 +4,7 @@ import android.graphics.Color
 
 class Malus(x:Float,y:Float,diametre:Float) : Effets(x,y,diametre){
     override var OnScreen = true
-    override val color = Color.rgb(132, 14, 78)
+    override val color = Color.rgb(253, 81, 0)
     override var incrementation_de_taille_x = -50f
     override var incrementation_de_vitesse = 200
 
