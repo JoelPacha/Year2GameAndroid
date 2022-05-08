1b217a9431753bb268db77bab3b8d1c24dc52e2d
Thomas Syti <thomas.syti@gmail.com>
2022-05-08 17:07:30 +0200

    ajout ost

--- app/src/main/java/com/example/projet/Menu.kt
+++ app/src/main/java/com/example/projet/Menu.kt
@@ -1,6 +1,7 @@
 package com.example.projet
 
 import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,8 +9,12 @@ import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_menu.*
 
 class Menu : AppCompatActivity(), View.OnClickListener{
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var mediaPlayer = MediaPlayer.create(this,R.raw.ost)
+        mediaPlayer.start()
         setContentView(R.layout.activity_menu)
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN) //remove le haut de l'écran(heure,batterie,etc..)
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION) //remove les boutons en bas de l'écran
