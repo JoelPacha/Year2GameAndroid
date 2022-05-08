28a7d13a824e680bbfc508050f80501819c42dca
fd06 <103423277+fd06@users.noreply.github.com>
2022-05-08 13:46:14 +0200

    niveau

---
+++ app/src/main/java/com/example/projet/niveau3.kt
@@ -0,0 +1,11 @@
+package com.example.projet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class niveau3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_niveau3)
+    }
+}
